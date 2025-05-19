4cb08295950ea63be82be0cc533b6a98d1acbcb0
ratingyou <christwithus7@proton.me>
2025-05-19 10:45:49 +1000

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -84,3 +84,11 @@ def analyze():
     }
 
     return jsonify(response)
+
+
+import os
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))  # fallback to 5000 for local dev
+    app.run(host='0.0.0.0', port=port)
+
